dab614a34cd08b8999beef3b10f38f4c404b9904
Nadiia Poshtova <feyanana29@gmail.com>
2025-04-22 20:07:23 +0300

    add general layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from '../components/layout/header/Header';
+import Sidebar from '../components/layout/sidebar/Sidebar';
+import LoginPage from '../pages/login/LoginPage';
+import styles from './App.module.scss';
+import HomePage from '../pages/login/HomePage';
 
 function App() {
 	return (
-		<>
+		<div className={styles.app}>
 			<Header />
-		</>
+			<Sidebar />
+
+			<main className={styles.main}>
+				<Routes>
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/' element={<HomePage />} />
+				</Routes>
+			</main>
+		</div>
 	);
 }
 
