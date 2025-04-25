4576c652065e334420f9650641b57090c9c9de0f
Nadiia Poshtova <feyanana29@gmail.com>
2025-04-25 11:14:02 +0300

    router guard auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from '../components/layout/header/Header';
 import Sidebar from '../components/layout/sidebar/Sidebar';
 import styles from './App.module.scss';
 import HomePage from '../pages/login/HomePage';
 import LoginPage from '../pages/login/LoginPage';
+import { useEffect, useState } from 'react';
+import { getAuth } from '../utils/auth';
 
 function App() {
+	const [isAuth, setIsAuth] = useState(false);
+
+	useEffect(() => {
+		setIsAuth(getAuth());
+	}, []);
+
 	return (
 		<div className={styles.app}>
 			<Header />
@@ -13,8 +21,14 @@ function App() {
 
 			<main className={styles.main}>
 				<Routes>
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/' element={<HomePage />} />
+					<Route
+						path='/login'
+						element={isAuth ? <Navigate to='/' replace /> : <LoginPage />}
+					/>
+					<Route
+						path='/'
+						element={isAuth ? <HomePage /> : <Navigate to='/login' replace />}
+					/>
 				</Routes>
 			</main>
 		</div>
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,3 +3,8 @@ export const AUTH_KEY = 'isAuth';
 export const setAuth = (value: boolean): void => {
 	localStorage.setItem(AUTH_KEY, JSON.stringify(value));
 };
+
+export const getAuth = (): boolean => {
+	const value = localStorage.getItem(AUTH_KEY);
+	return value ? JSON.parse(value) : false;
+};
