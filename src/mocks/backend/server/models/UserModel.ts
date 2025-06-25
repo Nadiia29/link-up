2b4d102ea25772515ccfbe5460cde94256ef443a
Ihor Kuchin <i.kuchin@yaware.com>
2025-06-25 18:43:29 +0300

    add get-me to mock back

--- src/mocks/backend/server/models/UserModel.ts
+++ src/mocks/backend/server/models/UserModel.ts
@@ -3,6 +3,7 @@ import { User } from '../types';
 export class UserModel implements User {
     constructor(
         public id: string,
+        public uuid: string,
         public name: string,
         public pass: string,
         public email: string,
--- src/mocks/backend/server/routes/meRoutes.ts
+++ src/mocks/backend/server/routes/meRoutes.ts
@@ -1,4 +1,9 @@
 import { http } from 'msw';
-import { createUser, getUser } from '../controllers/meController';
+import { isAuth } from '../isAuth';
+import { createMe, getTokenByEmail, getMe } from '../controllers/meController';
 
-export const meRoutes = [http.post('/api/register', createUser), http.post('/api/login', getUser)];
+export const meRoutes = [
+    http.post('/api/register', createMe),
+    http.post('/api/login', getTokenByEmail),
+    http.post('/api/me', isAuth(getMe)),
+];
